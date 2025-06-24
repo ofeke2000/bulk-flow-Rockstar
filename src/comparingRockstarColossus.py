9a2663348e61faf525b0040d12e373162c5b878b
Ofek Einav <ofeke@campus.technion.ac.il>
2025-06-24 16:05:26 +0300

    comment out camb and CLASS

--- src/comparingRockstarColossus.py
+++ src/comparingRockstarColossus.py
@@ -7,6 +7,7 @@ from classy import Class
 from scipy.integrate import quad
 from scipy.interpolate import interp1d
 from colossus.cosmology import cosmology
+from scipy.special import spherical_jn
 
 # Load Rockstar simulation results
 summary_csv_rockstar = os.path.expanduser("~/bulk-flow-Rockstar/Data/mdpl2_rockstar_125_pid-1_mvir12/bulk_flow_radius_series/bulk_flow_summary.csv")
@@ -38,9 +39,9 @@ cosmo = cosmology.getCurrent()
 # Growth rate at z=0
 f = cosmo.Om0**0.55
 
-# Window function (Fourier transform of spherical top-hat)
+# Define top-hat window function
 def W(k, R):
-    return 3 * (np.sin(k*R) - k*R * np.cos(k*R)) / ((k*R)**3 + 1e-10)
+    return 3 * spherical_jn(1, k*R) / (k*R)
 
 # ======================== THEORETICAL CALCULATIONS ========================
 # Define k values for integration
@@ -61,61 +62,61 @@ def bulk_flow_rms_colossus(R):
 
 theory_colossus = np.array([bulk_flow_rms_colossus(R) for R in radii_rockstar])
 
-# ----- CAMB calculation -----
-# Set up CAMB parameters
-pars = camb.CAMBparams()
-pars.set_cosmology(
-    H0=my_cosmo_params['H0'],
-    ombh2=my_cosmo_params['Ob0']*(my_cosmo_params['H0']/100)**2,
-    omch2=(my_cosmo_params['Om0']-my_cosmo_params['Ob0'])*(my_cosmo_params['H0']/100)**2
-)
-pars.InitPower.set_params(ns=my_cosmo_params['ns'])
-pars.set_matter_power(redshifts=[0], kmax=max(k_vals))
-results = camb.get_results(pars)
-kh, z, pk_camb = results.get_matter_power_spectrum(minkh=min(k_vals), maxkh=max(k_vals), npoints=len(k_vals))
-interp_pk_camb = interp1d(kh, pk_camb, bounds_error=False, fill_value=0)
-
-def integrand_camb(k, R):
-    pk = interp_pk_camb(k)
-    return pk * W(k, R)**2 * k**2
-
-def bulk_flow_rms_camb(R):
-    k_min = 1e-4
-    k_max = 10
-    integral, _ = quad(lambda k: integrand_camb(k, R), k_min, k_max, limit=200000)
-    H0 = my_cosmo_params['H0']
-    sigma_v = H0**2 * f**2 /(2 * np.pi**2) * integral
-    return np.sqrt(sigma_v)
-
-theory_camb = np.array([bulk_flow_rms_camb(R) for R in radii_rockstar])
-
-# ----- CLASS calculation -----
-params_class = {
-    'h': my_cosmo_params['H0']/100,
-    'omega_b': my_cosmo_params['Ob0']*(my_cosmo_params['H0']/100)**2,
-    'omega_cdm': (my_cosmo_params['Om0']-my_cosmo_params['Ob0'])*(my_cosmo_params['H0']/100)**2,
-    'A_s': 2.1e-9,  # Adjusted to match sigma8
-    'n_s': my_cosmo_params['ns'],
-    'output': 'mPk',
-    'P_k_max_h/Mpc': max(k_vals),
-}
-cosmo_class = Class()
-cosmo_class.set(params_class)
-cosmo_class.compute()
-pk_class = np.array([cosmo_class.pk(k, 0) for k in k_vals])
-interp_pk_class = interp1d(k_vals, pk_class, bounds_error=False, fill_value=0)
-
-def integrand_class(k, R):
-    pk = interp_pk_class(k)
-    return pk * W(k, R)**2 * k**2
-
-def bulk_flow_rms_class(R):
-    k_min = 1e-4
-    k_max = 10
-    integral, _ = quad(lambda k: integrand_class(k, R), k_min, k_max, limit=200000)
-    H0 = my_cosmo_params['H0']
-    sigma_v = H0**2 * f**2 /(2 * np.pi**2) * integral
-    return np.sqrt(sigma_v)
+# # ----- CAMB calculation -----
+# # Set up CAMB parameters
+# pars = camb.CAMBparams()
+# pars.set_cosmology(
+#     H0=my_cosmo_params['H0'],
+#     ombh2=my_cosmo_params['Ob0']*(my_cosmo_params['H0']/100)**2,
+#     omch2=(my_cosmo_params['Om0']-my_cosmo_params['Ob0'])*(my_cosmo_params['H0']/100)**2
+# )
+# pars.InitPower.set_params(ns=my_cosmo_params['ns'])
+# pars.set_matter_power(redshifts=[0], kmax=max(k_vals))
+# results = camb.get_results(pars)
+# kh, z, pk_camb = results.get_matter_power_spectrum(minkh=min(k_vals), maxkh=max(k_vals), npoints=len(k_vals))
+# interp_pk_camb = interp1d(kh, pk_camb, bounds_error=False, fill_value=0)
+#
+# def integrand_camb(k, R):
+#     pk = interp_pk_camb(k)
+#     return pk * W(k, R)**2 * k**2
+#
+# def bulk_flow_rms_camb(R):
+#     k_min = 1e-4
+#     k_max = 10
+#     integral, _ = quad(lambda k: integrand_camb(k, R), k_min, k_max, limit=200000)
+#     H0 = my_cosmo_params['H0']
+#     sigma_v = H0**2 * f**2 /(2 * np.pi**2) * integral
+#     return np.sqrt(sigma_v)
+#
+# theory_camb = np.array([bulk_flow_rms_camb(R) for R in radii_rockstar])
+#
+# # ----- CLASS calculation -----
+# params_class = {
+#     'h': my_cosmo_params['H0']/100,
+#     'omega_b': my_cosmo_params['Ob0']*(my_cosmo_params['H0']/100)**2,
+#     'omega_cdm': (my_cosmo_params['Om0']-my_cosmo_params['Ob0'])*(my_cosmo_params['H0']/100)**2,
+#     'A_s': 2.1e-9,  # Adjusted to match sigma8
+#     'n_s': my_cosmo_params['ns'],
+#     'output': 'mPk',
+#     'P_k_max_h/Mpc': max(k_vals),
+# }
+# cosmo_class = Class()
+# cosmo_class.set(params_class)
+# cosmo_class.compute()
+# pk_class = np.array([cosmo_class.pk(k, 0) for k in k_vals])
+# interp_pk_class = interp1d(k_vals, pk_class, bounds_error=False, fill_value=0)
+#
+# def integrand_class(k, R):
+#     pk = interp_pk_class(k)
+#     return pk * W(k, R)**2 * k**2
+#
+# def bulk_flow_rms_class(R):
+#     k_min = 1e-4
+#     k_max = 10
+#     integral, _ = quad(lambda k: integrand_class(k, R), k_min, k_max, limit=200000)
+#     H0 = my_cosmo_params['H0']
+#     sigma_v = H0**2 * f**2 /(2 * np.pi**2) * integral
+#     return np.sqrt(sigma_v)
 
 theory_class = np.array([bulk_flow_rms_class(R) for R in radii_rockstar])
 
